53b055404abb5c9a265e18a964f31bc98c9e8adb
huangdongcheng <rocky114!QAZ>
2021-09-07 17:26:14 +0800

    web framework

--- web/engine.go
+++ web/engine.go
@@ -1,7 +1,38 @@
 package web
 
+import (
+	"log"
+	"net/http"
+)
+
 type HandlerFunc func(ctx *Context)
 
 type Engine struct {
 	router *router
+}
+
+func New() *Engine {
+	return &Engine{router: newRouter()}
+}
+
+func (engine *Engine) addRoute(method, pattern string, handler HandlerFunc) {
+	log.Printf("Route %4s - %s", method, pattern)
+	engine.router.addRoute(method, pattern, handler)
+}
+
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
