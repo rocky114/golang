f0dacd8555d30c2dfd4e7e4526ae39c9278d5022
rocky114 <934313141@qq.com>
2021-09-09 23:19:03 +0800

    web framework

--- main.go
+++ main.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"go-example/web"
 	"net/http"
 )
 
 func main() {
-	fmt.Println("main")
-
 	r := web.New()
-	r.GET("/", func(ctx *web.Context) {
-		ctx.HTML(http.StatusOK, "<h1>Hello</h1>")
+	r.GET("/index", func(c *web.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *web.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+		})
 
-	r.GET("/hello", func(ctx *web.Context) {
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Query("name"), ctx.Path)
-	})
+		v1.GET("/hello", func(c *web.Context) {
+			// expect /hello?name=geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *web.Context) {
+			// expect /hello/geektutu
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *web.Context) {
+			c.JSON(http.StatusOK, web.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(ctx *web.Context) {
-		ctx.String(http.StatusOK, "hello %s, you're at %s\n", ctx.Params["name"], ctx.Path)
-	})
+	}
 
-	r.GET("/assets/*filepath", func(ctx *web.Context) {
-		ctx.JSON(http.StatusOK, web.H{"filepath": ctx.Params["filepath"]})
-	})
-
-	r.Run(":8888")
+	r.Run(":9999")
 }
--- web/engine.go
+++ web/engine.go
@@ -1,21 +1,56 @@
 package web
 
 import (
-	"log"
 	"net/http"
 )
 
 type HandlerFunc func(ctx *Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	engine      *Engine
+}
+
 type Engine struct {
 	router *router
+	groups []*RouterGroup
+	*RouterGroup
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+
+	return engine
 }
 
-func (engine *Engine) addRoute(method, pattern string, handler HandlerFunc) {
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+
+	return newGroup
+}
+
+func (group *RouterGroup) addRoute(method, pattern string, handler HandlerFunc) {
+	pattern = group.prefix + pattern
+	group.engine.router.addRoute(method, pattern, handler)
+}
+
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
+}
+
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
+}
+
+/*func (engine *Engine) addRoute(method, pattern string, handler HandlerFunc) {
 	log.Printf("Route %4s - %s", method, pattern)
 	engine.router.addRoute(method, pattern, handler)
 }
@@ -26,7 +61,7 @@ func (engine *Engine) GET(pattern string, handler HandlerFunc) {
 
 func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 	engine.addRoute("POST", pattern, handler)
-}
+}*/
 
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
