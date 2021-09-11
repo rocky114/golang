842d912bc34b5f798e73a66bcdba99bed3c5eee4
rocky114 <934313141@qq.com>
2021-09-11 22:59:47 +0800

    web framework

--- main.go
+++ main.go
@@ -2,38 +2,33 @@ package main
 
 import (
 	"go-example/web"
+	"log"
 	"net/http"
+	"time"
 )
+
+func onlyForV2() web.HandlerFunc {
+	return func(ctx *web.Context) {
+		t := time.Now()
+
+		log.Printf("[%d] %s in %v for group v2", ctx.StatusCode, ctx.Req.RequestURI, time.Since(t))
+	}
+}
 
 func main() {
 	r := web.New()
-	r.GET("/index", func(c *web.Context) {
-		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	r.Use(web.Logger()) // global midlleware
+	r.GET("/", func(c *web.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
-	v1 := r.Group("/v1")
-	{
-		v1.GET("/", func(c *web.Context) {
-			c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
-		})
 
-		v1.GET("/hello", func(c *web.Context) {
-			// expect /hello?name=geektutu
-			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-		})
-	}
 	v2 := r.Group("/v2")
+	v2.Use(onlyForV2()) // v2 group middleware
 	{
 		v2.GET("/hello/:name", func(c *web.Context) {
 			// expect /hello/geektutu
 			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 		})
-		v2.POST("/login", func(c *web.Context) {
-			c.JSON(http.StatusOK, web.H{
-				"username": c.PostForm("username"),
-				"password": c.PostForm("password"),
-			})
-		})
-
 	}
 
 	r.Run(":9999")
