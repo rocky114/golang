b1eb69224fcbd466b043f8e081758ed06f10d6b9
rocky114 <934313141@qq.com>
2021-11-25 10:31:46 +0800

    web framework, middleware

--- framework/engine.go
+++ framework/engine.go
@@ -6,41 +6,32 @@ import (
 
 type handlerFunc func(ctx *Context)
 
-type engine struct {
+type routerGroup struct {
+	prefix     string
 	router     *router
 	middleware *middleware
-	*routerGroup
 }
 
-type routerGroup struct {
-	prefix string
-	engine *engine
-}
-
-func New() *engine {
-	e := &engine{router: newRouter()}
-	e.routerGroup = &routerGroup{engine: e}
-	e.middleware = &middleware{}
-
-	return e
+func New() *routerGroup {
+	return &routerGroup{router: newRouter(), middleware: &middleware{}}
 }
 
 func (group *routerGroup) Use(middlewares ...handlerFunc) {
-	group.engine.middleware.insert(parsePattern(group.prefix), 0, middlewares)
+	group.middleware.insert(parsePattern(group.prefix), 0, middlewares)
 }
 
 func (group *routerGroup) Group(prefix string) *routerGroup {
-	e := group.engine
 	newGroup := &routerGroup{
-		prefix: group.prefix + prefix,
-		engine: e,
+		prefix:     group.prefix + prefix,
+		router:     group.router,
+		middleware: group.middleware,
 	}
 
 	return newGroup
 }
 
 func (group *routerGroup) addRoute(method string, pattern string, handler handlerFunc) {
-	group.engine.router.addRoute(method, group.prefix+pattern, handler)
+	group.router.addRoute(method, group.prefix+pattern, handler)
 }
 
 func (group *routerGroup) GET(pattern string, handler handlerFunc) {
@@ -51,16 +42,16 @@ func (group *routerGroup) POST(pattern string, handler handlerFunc) {
 	group.addRoute("POST", pattern, handler)
 }
 
-func (engine *engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	middlewares := engine.middleware.find(parsePattern(req.URL.Path), 0)
+func (group *routerGroup) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	middlewares := group.middleware.find(parsePattern(req.URL.Path), 0)
 
 	middlewares = append(middlewares, func(ctx *Context) {
-		engine.router.handle(ctx)
+		group.router.handle(ctx)
 	})
 
 	newContext(w, req, middlewares).Next()
 }
 
-func (engine *engine) Run(addr string) error {
-	return http.ListenAndServe(addr, engine)
+func (group *routerGroup) Run(addr string) error {
+	return http.ListenAndServe(addr, group)
 }
