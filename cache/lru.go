4d8463d4cc2f6c6f08a46744a5a2c24331903f08
rocky114 <934313141@qq.com>
2021-12-02 20:35:16 +0800

    cache, LRU implement

--- cache/lru.go
+++ cache/lru.go
@@ -1,22 +1,33 @@
 package cache
 
-import "container/list"
+import (
+	"container/list"
+	"reflect"
+	"sync"
+	"unsafe"
+)
 
 type Cache struct {
-	maxBytes int
-	list     *list.List
-	entry    map[string]*list.Element
+	total uint
+	used  uint
+	list  *list.List
+	entry map[string]*list.Element
+	mu    *sync.Mutex
 }
 
-func NewCache(size int) *Cache {
+func NewCache(total uint) *Cache {
 	return &Cache{
-		maxBytes: size,
-		list:     list.New(),
-		entry:    make(map[string]*list.Element),
+		total: total,
+		list:  list.New(),
+		entry: make(map[string]*list.Element),
+		mu:    &sync.Mutex{},
 	}
 }
 
 func (c *Cache) Get(key string) interface{} {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
 	if element, ok := c.entry[key]; ok {
 		return element.Value
 	}
@@ -25,6 +36,11 @@ func (c *Cache) Get(key string) interface{} {
 }
 
 func (c *Cache) Set(key string, val interface{}) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.free(key)
+
 	if element, ok := c.entry[key]; ok {
 		element.Value = val
 		c.list.MoveToFront(element)
@@ -33,13 +49,54 @@ func (c *Cache) Set(key string, val interface{}) bool {
 		c.entry[key] = c.list.PushFront(val)
 	}
 
+	c.used += uint(len(key)) + length(val)
+
 	c.evict()
 
 	return true
 }
 
+func length(val interface{}) uint {
+	kind := reflect.TypeOf(val).Kind()
+	if kind == reflect.Array || kind == reflect.String || kind == reflect.Chan || kind == reflect.Map || kind == reflect.Slice {
+		return uint(reflect.ValueOf(val).Len())
+	}
+
+	return uint(unsafe.Sizeof(val))
+}
+
+func (c *Cache) free(key string) {
+	if _, ok := c.entry[key]; !ok {
+		return
+	}
+
+	c.used -= length(key)
+
+	c.used -= length(c.entry[key].Value)
+}
+
+func (c *Cache) Delete(key string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.free(key)
+
+	if _, ok := c.entry[key]; !ok {
+		return false
+	}
+
+	c.list.Remove(c.entry[key])
+	delete(c.entry, key)
+
+	return true
+}
+
+func (c *Cache) Len() int {
+	return c.list.Len()
+}
+
 func (c *Cache) isFull() bool {
-	if c.list.Len() >= c.maxBytes {
+	if c.used >= c.total {
 		return true
 	}
 
@@ -52,15 +109,11 @@ func (c *Cache) evict() bool {
 	}
 
 	c.list.Remove(c.list.Back())
-	c.purge()
-
-	return true
-}
-
-func (c *Cache) purge() {
 	for key, val := range c.entry {
 		if val == nil {
 			delete(c.entry, key)
 		}
 	}
+
+	return true
 }
--- cache/lru_test.go
+++ cache/lru_test.go
@@ -1,10 +1,15 @@
 package cache
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGet(t *testing.T) {
 	cache := NewCache(200)
 	cache.Set("name", "rocky")
+	cache.Set("age", 1)
+
+	t.Log(cache.Len(), 3)
 
 	if cache.Get("name") != "rocky" {
 		t.Error("cache error")
